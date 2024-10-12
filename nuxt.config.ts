b0b07de7248c5dca3aa650464afd8d8b81466f99
Marek Pribyl <mr.pribyl@gmail.com>
2024-10-12 19:38:00 +0200

    Page title fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,19 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Lazy Rabbit"
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ["v-network-graph/lib/style.css"],
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/content'
-  ]
+  ],
+  content: {
+    contentHead: false
+  }
 })
